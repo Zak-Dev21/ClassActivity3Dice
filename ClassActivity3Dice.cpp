15d2c2f795259162575935ee7c226ae73c9dc13a
Zak <zakariabouaddou@gmail.com>
2024-09-13 21:04:18 -0700

    Fixed Syntax issue, now generating use of rand in c++

--- ClassActivity3Dice.cpp
+++ ClassActivity3Dice.cpp
@@ -3,10 +3,16 @@
 
 int main()
 {
+    long ElapsedSeconds = time(0);
     int minValue = 1;
     int maxValue = 6;
 
-    int firstValue = (rand() % (maxValue – minValue + 1)) + minValue;
+    int firstValue = (rand() % (maxValue - minValue + 1)) + minValue;
 
-    int secondValue = [rand() % (maxValue – minValue + 1)] + minValue;
+    int secondValue = (rand() % (maxValue - minValue + 1)) + minValue;
+
+	std::cout << "First dice value: " << firstValue << std::endl;
+	std::cout << "Second dice value: " << secondValue << std::endl;
+
+	return 0;
 }
